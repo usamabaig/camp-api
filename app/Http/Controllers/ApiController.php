df81d95131756a1710c4054e1e9007cae03efd7c
Usama <muhammad.baig@linentech.net>
2019-11-26 21:32:39 +0500

    login api added

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ApiController extends Controller
+{
+    /**
+     * Login User
+     */
+    public function login(Request $request)
+    {
+        if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){
+            Auth::attempt(['email' => $request->email, 'password' => $request->password]);
+            $success['user'] = Auth::user();
+
+            return response()->json(['success' => $success]);
+        } else {
+
+            return response()->json(['error'=>'Username/Password is incorrect'], 401);
+        }
+    }
+}
