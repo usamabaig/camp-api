d5e523e36fc850b8e64b710192066827ff57296c
Usama <muhammad.baig@linentech.net>
2019-12-16 23:32:25 +0500

    camp api

--- app/Http/Controllers/CampController.php
+++ app/Http/Controllers/CampController.php
@@ -10,7 +10,7 @@ class CampController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function index()
     {
@@ -33,22 +33,26 @@ class CampController extends Controller
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function store(Request $request)
     {
-        //
+        $this->createOrUpdateCamp($request, null);
+
+        return response()->json(['msg' => 'Camp Created Successfully'], 200);
     }
 
     /**
      * Display the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function show($id)
     {
-        //
+        $camp = Camp::where('id', $id)->first();
+
+        return response()->json($camp);
     }
 
     /**
@@ -67,31 +71,36 @@ class CampController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function update(Request $request, $id)
     {
-        //
+        $this->createOrUpdateCamp($request, $id);
+
+        return response()->json(['msg' => 'Camp Updated Successfully'], 200);
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function destroy($id)
     {
-        //
+        Camp::where('id', $id)->delete();
+
+        return response()->json(['msg' => 'Camp Deleted Successfully'], 200);
     }
 
-    private function createCamp($request, $id = 0)
+    private function createOrUpdateCamp($request, $id = null)
     {
-        $camp = new Camp();
+        $camp = isset($id) ? Camp::find($id) : new Camp();
         $camp->camp_name = $request->camp_name;
         $camp->camp_type = $request->camp_type;
         $camp->dr_name = $request->dr_name;
         $camp->camp_datetime = date("Y-m-d H:i:s", strtotime($request->camp_datetime));
+        $camp->address = $request->address;
         $camp->lat = $request->lat;
         $camp->lng = $request->lng;
         $camp->user_id = $request->user_id;
@@ -100,5 +109,6 @@ class CampController extends Controller
         $camp->no_of_strips = $request->no_of_strips;
         $camp->no_of_flyers = $request->no_of_flyers;
         $camp->no_of_screening_slips = $request->no_of_screening_slips;
+        $camp->save();
     }
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,7 +11,7 @@ class UserController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function index()
     {
@@ -34,7 +34,7 @@ class UserController extends Controller
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function store(Request $request)
     {
@@ -58,7 +58,7 @@ class UserController extends Controller
      * Display the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function show($id)
     {
@@ -83,7 +83,7 @@ class UserController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function update(Request $request, $id)
     {
@@ -107,7 +107,7 @@ class UserController extends Controller
      * Remove the specified resource from storage.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function destroy($id)
     {
