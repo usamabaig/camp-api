1cea177061acc4c2d07599ae93fd3b8276a0f268
Usama <muhammad.baig@linentech.net>
2019-12-17 22:13:47 +0500

    database config updated

--- config/database.php
+++ config/database.php
@@ -1,7 +1,20 @@
 <?php
 
 use Illuminate\Support\Str;
+$env = getenv('APP_ENV');
+if($env == 'prod') {
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
+    $host = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $database = substr($url["path"], 1);
+}else {
+    $host = env('DB_HOST', '127.0.0.1');
+    $username = env('DB_USERNAME', 'forge');
+    $password = env('DB_PASSWORD', '');
+    $database = env('DB_DATABASE', 'forge');
+}
 return [
 
     /*
@@ -46,11 +59,11 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => $host,
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
