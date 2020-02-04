8bad770926230a59d4dbb7f93f85f59243650b86
usamabaig <usamabaig8@gmail.com>
2020-02-05 01:16:55 +0500

    fix issue

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -27,8 +27,8 @@ class ReportsController extends Controller
 
     public function getPreviousCamps(Request $request)
     {
-        $to_date = isset($request->startDate) ? date('Y-m-d H:i:s', strtotime($request->startDate)) : date('Y-m-d H:i:s',strtotime("-1 days"));
-        $from_date = isset($request->endDate) ? date('Y-m-d H:i:s', strtotime($request->endDate)) : date('Y-m-d H:i:s',strtotime("-1 year"));
+        $to_date = isset($request->endDate) ? date('Y-m-d H:i:s', strtotime($request->endDate)) : date('Y-m-d H:i:s',strtotime("-1 days"));
+        $from_date = isset($request->startDate) ? date('Y-m-d H:i:s', strtotime($request->startDate)) : date('Y-m-d H:i:s',strtotime("-1 year"));
         $date = [$from_date, $to_date];
         $camp = Camp::with('user', 'user.user_territory', 'user.user_district', 'user.user_region', 'user.user_team')->where(function ($query) use ($request) {
             if (isset($request->doctorName)){
