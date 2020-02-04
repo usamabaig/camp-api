9a93bcc83cb7519c36ba127d8f62b685a52338e0
usamabaig <usamabaig8@gmail.com>
2020-02-05 01:54:01 +0500

    fix issues

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -8,25 +8,6 @@ class ReportsController extends Controller
 {
     public function getPresentCamps(Request $request)
     {
-        $camp = Camp::with('user', 'user.user_territory', 'user.user_district', 'user.user_region', 'user.user_team')->where(function ($query) use ($request) {
-            if (isset($request->doctorName)){
-                $query->where('dr_name', 'like', '%'.$request->doctorName.'%');
-            }
-        })->where(function ($query) use ($request) {
-            if (isset($request->campType)){
-                $query->where('camp_type', '=', $request->campType);
-            }
-        })->where(function ($query) use ($request) {
-            if (isset($request->campStatus)){
-                $query->where('camp_status', '=', $request->campStatus);
-            }
-        })->get();
-
-        return response()->json($camp);
-    }
-
-    public function getPreviousCamps(Request $request)
-    {
         $to_date = isset($request->endDate) ? date('Y-m-d H:i:s', strtotime($request->endDate)) : date('Y-m-d H:i:s',strtotime("-1 days"));
         $from_date = isset($request->startDate) ? date('Y-m-d H:i:s', strtotime($request->startDate)) : date('Y-m-d H:i:s',strtotime("-1 year"));
         $date = [$from_date, $to_date];
