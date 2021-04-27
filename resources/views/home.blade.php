00a6a3f2a1d821566aab051cfa2b03916325d117
usama-dev <>
2021-04-27 18:04:07 +0500

    team condition added on listing

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,20 +3,35 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-4">
+        <div class="col-md-4 mb-3">
             <div class="card">
-                @if(auth()->user()->id == 1)
                 <div class="card-header">Deleted Users</div>
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
                     <p>All deleted users will be listed here to restore or delete permanently</p>
                     <a class="btn btn-primary btn-sm" href="{{ route('user.deleted') }}">View</a>
                 </div>
-                @endif
+            </div>
+        </div>
+        @foreach($teams as $team)
+            <div class="col-md-4 mb-3">
+                <div class="card">
+                    <div class="card-header"><b>{{ $team->team_name }} Team</b></div>
+                    <div class="card-body">
+                        <p>All Regions, Districts and territories belonging to {{ $team->team_name }} Team will be listed here.</p>
+                        <a class="btn btn-primary btn-sm" href="{{ route('team.details', [$team->id, 'region']) }}">Regions</a>
+                        <a class="btn btn-success btn-sm" href="{{ route('team.details', [$team->id, 'district']) }}">Districts</a>
+                        <a class="btn btn-dark btn-sm" href="{{ route('team.details', [$team->id, 'territory']) }}">Territories</a>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+        <div class="col-md-4 mb-3">
+            <div class="card">
+                <div class="card-header">Import Doctors</div>
+                <div class="card-body">
+                    <p>Import doctors from excel file.</p>
+                    <a class="btn btn-primary btn-sm" href="{{ route('doctor') }}">View</a>
+                </div>
             </div>
         </div>
     </div>
