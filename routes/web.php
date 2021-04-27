8a8f72087cd4870127f258bc0e329559297ec190
usama-dev <>
2021-04-27 18:03:46 +0500

    team condition added on listing

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,9 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/user/deleted', 'HomeController@viewDeletedUsers')->name('user.deleted');
 Route::get('/user/restore/{id}', 'HomeController@restoreUser')->name('user.restore');
 Route::get('/user/delete/permanent/{id}', 'HomeController@hardDeleteUser')->name('user.permanent.delete');
+
+Route::get('/team/{team_id}/{show}', 'HomeController@teamDetails')->name('team.details');
+Route::post('/add/region', 'HomeController@addData')->name('add.data');
+
+Route::get('/doctor', 'HomeController@viewDoctors')->name('doctor');
+Route::post('/import/doctors', 'HomeController@importDoctors');
